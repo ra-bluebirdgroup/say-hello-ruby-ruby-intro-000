e5656158fc14a46289810bef167d0f221462991a
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-11 18:11:08 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,5 @@
 # Build your say_hello method here
-def say_hello(name, language = "Ruby Programmer")
-  puts "Hello #{name} you are a great #{language}"
+def say_hello(name  = "Ruby Programmer")
+  puts "Hello #{name}"}
 
 end
